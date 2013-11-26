d356a7b96296f2268f15d5973b5b76f888ba60cc
NewGyu <s.nisikw+git@gmail.com>
2013-11-26 22:53:47 +0900

    initial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import views.html.defaultpages.badRequest
+
+object Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+  def sayHello(name:String, age:Long) = Action{request =>
+    Ok(Json.obj("type" ->"AAA", "name"->name, "age"->age))
+  }
+
+}
